c4d10a2a31717730f8227b84929af8ce6a2d6f03
Ole Aamot <ole@aamotsoftware.com>
2019-01-14 05:24:07 +0100

    Add Location Manager

--- ios/DataViewController.swift
+++ ios/DataViewController.swift
@@ -7,28 +7,91 @@
 //
 
 import UIKit
+import MapKit
 
-class DataViewController: UIViewController {
-
+class DataViewController : UIViewController {
+    let locationManager = CLLocationManager()
     @IBOutlet weak var dataLabel: UILabel!
     var dataObject: String = ""
-
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.requestLocation()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        self.dataLabel!.text = dataObject
-    }
-
-
 }
 
+extension DataViewController : CLLocationManagerDelegate {
+   private func locationManager(locationManager: CLLocationManager, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
+        if status == .authorizedWhenInUse {
+            locationManager.requestLocation()
+        }
+    }
+    
+    private func locationManager(locationManager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.first {
+            print("location:: \(location)")
+        }
+    }
+    
+    private func locationManager(locationManager: CLLocationManager, didFailWithError error: NSError) {
+        print("error:: \(error)")
+    }
+}
+
+//class DataViewController: UIViewController {
+
+//    let locationManager = CLLocationManager()
+
+//    @IBOutlet weak var dataLabel: UILabel!
+//    var dataObject: String = ""
+
+  //  override func viewDidLoad() {
+    //    super.viewDidLoad()
+
+      //  locationManager.delegate = self
+       // locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        //locationManager.requestWhenInUseAuthorization()
+        //locationManager.requestLocation()
+
+        // 1)
+        // MapView.mapType = MKMapType.Standard
+        
+        // 2)
+        //let location = CLLocationCoordinate2D(latitude: 23.0225,longitude:
+        // 72.5714)
+        
+        // 3)
+        //let span = MKCoordinateSpanMake(0.05, 0.05)
+        //let region = MKCoordinateRegion(center: location, span: span)
+        // MapView.setRegion(region, animated: true)
+        
+        // 4)
+        //let annotation = MKPointAnnotation()
+        //annotation.coordinate = location
+        //annotation.title = "Piperpal"
+        //annotation.subtitle = "FriendlyCoffeeShop"
+        // MapView.addAnnotation(annotation)
+
+        // Do any additional setup after loading the view, typically from a nib.
+    //}
+
+    //let regionRadius: CLLocationDistance = 1000
+    //func centerMapOnLocation(location: CLLocation) {
+    //    let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate,
+        //
+// regionRadius, regionRadius)
+        // MapView.setRegion(coordinateRegion, animated: true)
+//    }
+    
+  //  override func didReceiveMemoryWarning() {
+    //    super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    //}
+
+    //override func viewWillAppear(_ animated: Bool) {
+     //   super.viewWillAppear(animated)
+     //   self.dataLabel!.text = dataObject
+    //}
+//}
