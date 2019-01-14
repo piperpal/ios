e12eb75c16ba5cab8200da021e625af27ec4bf34
Ole Aamot <ole@aamotsoftware.com>
2019-01-14 04:26:06 +0100

    Piperpal for iOS 2019-01-14

---
+++ ios/DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  ios
+//
+//  Created by Ole Aamot on 1/14/19.
+//  Copyright © 2019 Aamot Software. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
